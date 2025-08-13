114c720b067e173d990514f0c9130ac5e9e2babf
MikkoParkkola <78788115+MikkoParkkola@users.noreply.github.com>
2025-08-13 03:22:33 +0200

    refactor: centralize session storage utility

--- test/utils.test.js
+++ test/utils.test.js
@@ -29,15 +29,21 @@ test('getRoomDir returns sanitized directory path', () => {
   assert.ok(fs.existsSync(roomDir));
 });
 
-test('FileSessionStore persists values', () => {
+test('FileSessionStore implements basic Storage API', () => {
   cleanup();
   const storePath = path.join(tmpBase, 'session.json');
   const store = new FileSessionStore(storePath);
+  assert.strictEqual(store.length, 0);
   assert.strictEqual(store.getItem('foo'), null);
   store.setItem('foo', 'bar');
+  store.setItem('baz', 'qux');
+  assert.strictEqual(store.length, 2);
+  assert.strictEqual(store.key(0), 'foo');
   assert.strictEqual(store.getItem('foo'), 'bar');
   store.removeItem('foo');
-  assert.strictEqual(store.getItem('foo'), null);
+  assert.strictEqual(store.length, 1);
+  store.clear();
+  assert.strictEqual(store.length, 0);
 });
 
 test.after(() => {
