19d907476ef3b2e940c3b3b3b0366da256193507
MikkoParkkola <78788115+MikkoParkkola@users.noreply.github.com>
2025-08-18 07:01:14 +0200

    Use gosu for node execution (#160)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-set -e
+set -euo pipefail
 
 # Ensure writable directories for node user
 chown -R node:node /app/mx-cache /app/room-logs
 
-# Run the provided command as the node user
-exec su node -s /bin/sh -c "$*"
 exec gosu node "$@"
